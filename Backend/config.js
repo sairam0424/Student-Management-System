1de1cd2c4cc069e669607d133310b246eeed214d
sairam0424 <uggesairam0000@gmail.com>
2024-08-26 20:39:39 +0530

    feat(backend):added Signup and Login Mutation using the graphQl mutations added the signin and login mutation and test using the playground and also added the jwt tokens to it

--- Backend/config.js
+++ Backend/config.js
@@ -1,1 +1,2 @@
 export const MONGO_URL="mongodb+srv://Sairam0000:Sairam0000@cluster0.0jd1v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+export const JWT_SECRET = "secret123"
--- Backend/models/UserModel.js
+++ Backend/models/UserModel.js
@@ -18,10 +18,6 @@ const UserSchema = new mongoose.Schema({
 
         type:String,
         required:true
-    },
-    role:{
-        type:String,
-        required:true
     }
 })
 
