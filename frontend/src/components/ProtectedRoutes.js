5164bbf6ac7185f799548277d175dbba91842fc2
sairam0424 <uggesairam0000@gmail.com>
2024-09-01 19:44:42 +0530

    perf(Ui):Improved the protected Routes using the usememo and fragment binding used the react memo to memoize the value of the memoize value of user and allowedRoles checks to prevent unneccesary computations

--- frontend/src/components/ProtectedRoutes.js
+++ frontend/src/components/ProtectedRoutes.js
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Navigate } from "react-router-dom";
 import { useUser } from "../customHooks/UserContext";
 
 const ProtectedRoute = ({ element: Component, allowedRoles }) => {
   const { user } = useUser();
 
+  const isAllowed = useMemo(() => {
+    if (!user) return false;
+    return allowedRoles.includes(user.role);
+  }, [user, allowedRoles]);
+
+  // Redirect if user is not logged in
   if (!user) {
     return <Navigate to="/login" replace />;
   }
 
-  if (!allowedRoles.includes(user.role)) {
+  // Redirect if user role is not allowed
+  if (!isAllowed) {
     return <Navigate to="/" replace />;
   }
 
-  return <Component />;
+  return <><Component /></>;
 };
 
 export default ProtectedRoute;
